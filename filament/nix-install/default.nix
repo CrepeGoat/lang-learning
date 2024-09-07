d7fbe931d6afec4f6ad94f505c77893649a92220
CrepeGoat <awqatty.b@gmail.com>
2024-09-07 13:10:44 -0600

    changed `package.nix` to accept arbitrary path for source code
    tested with both a `fetchFromGithub` path and a local path

--- filament/nix-install/default.nix
+++ filament/nix-install/default.nix
@@ -1,6 +1,15 @@
 let
   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05";
   pkgs = import nixpkgs { };
+  src = pkgs.fetchFromGitHub {
+    owner = "cucapra";
+    repo = "filament";
+    rev = "bb771f1b470b31178b68d238844995de159b7f30";
+    hash = "sha256-pHG0H4e6DieZSV+qiDHcmq1i4+CdqDJOfIAQnGmxba8=";
+  };
   runt = pkgs.callPackage ./runt/package.nix { };
 in
-pkgs.callPackage ./package.nix { runt = runt; }
+pkgs.callPackage ./package.nix {
+  src = src;
+  runt = runt;
+}
--- filament/nix-install/package.nix
+++ filament/nix-install/package.nix
@@ -1,8 +1,9 @@
 {
+  # source code repo
+  src,
   # nix stdlib
   lib,
   rustPlatform,
-  fetchFromGitHub,
   # dependencies
   python3,
   python3Packages,
@@ -12,43 +13,46 @@
   cvc5,
   whichSmt ? "z3",
 }:
-if (whichSmt != "z3" && whichSmt != "cvc5") then
-  throw "invalid value for `whichSmt`; must be either `z3` or `cvc5`"
-else
-  rustPlatform.buildRustPackage {
-    pname = "filament";
-    version = "0.1.0";
 
-    src = fetchFromGitHub {
-      owner = "cucapra";
-      repo = "filament";
-      rev = "bb771f1b470b31178b68d238844995de159b7f30";
-      hash = "sha256-pHG0H4e6DieZSV+qiDHcmq1i4+CdqDJOfIAQnGmxba8=";
-    };
+let
+  srcTomlMeta = (builtins.fromTOML (builtins.readFile "${src}/Cargo.toml")).workspace.package;
+  smt =
+    if (whichSmt == "z3") then
+      z3
+    else if (whichSmt == "cvc5") then
+      cvc5
+    else
+      throw "invalid value for `whichSmt`; must be either \"z3\" or \"cvc5\"";
+in
+rustPlatform.buildRustPackage {
+  pname = "filament";
+  version = srcTomlMeta.version;
 
-    cargoPatches = [ ./add-Cargo.lock.patch ];
-    cargoHash = "sha256-VMsjEP4/ivVe2UrXuZvMuiEhIr8RruHOX/e2kL0CJgU=";
+  src = src;
+  cargoPatches = [ ./add-Cargo.lock.patch ];
+  cargoHash = "sha256-VMsjEP4/ivVe2UrXuZvMuiEhIr8RruHOX/e2kL0CJgU=";
 
-    nativeBuildInputs = [
-      python3
-      python3Packages.numpy
-      runt
-      jq
-    ] ++ (if (whichSmt == "z3") then [ z3 ] else [ cvc5 ]);
+  nativeBuildInputs = [
+    python3
+    python3Packages.numpy
+    runt
+    jq
+    smt
+  ];
 
-    patches = [ ./runt.toml.patch ];
-    doCheck = false;
-    doInstallCheck = true;
-    installCheckPhase = ''
-      ln -s $out ./result
-      runt -d
-      rm ./result
-    '';
+  patches = [ ./runt.toml.patch ];
+  doCheck = false;
+  doInstallCheck = true;
+  installCheckPhase = ''
+    ln -s $out ./result
+    runt -d
+    rm ./result
+  '';
 
-    meta = with lib; {
-      description = "Fearless hardware design";
-      homepage = "https://filamenthdl.com";
-      license = licenses.mit;
-      platforms = platforms.all;
-    };
-  }
+  meta = {
+    description = srcTomlMeta.description;
+    homepage = srcTomlMeta.homepage;
+    license = lib.licenses.mit;
+    platforms = lib.platforms.all;
+  };
+}
